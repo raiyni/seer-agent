74e102a75eb197b4e04e5cc81ab34514cb95bb4e
Ron Young <admin@ryoung.io>
2023-01-26 19:00:29 -0600

    multistage build and get container data

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,17 @@
-FROM golang:1.19.5-alpine
-
-WORKDIR /app
+FROM golang:1.19.5-alpine as builder
+WORKDIR /go/src/github.com/raiyni/seer-agent
 
 COPY go.mod ./
+COPY go.sum ./
 COPY *.go ./
 
+RUN go install
 RUN go build -o /seer-agent
 
+
+FROM alpine:3.17
+COPY --from=builder /seer-agent ./
+
+VOLUME ["/var/run/docker.sock"]
 EXPOSE 3333
 CMD [ "/seer-agent" ]
--- main.go
+++ main.go
@@ -1,33 +1,62 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"io"
 	"net/http"
-	"os"
+
+	"context"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/labstack/echo/v4"
 )
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
+type Node struct {
+	Image  string            `json:"Image"`
+	Names  []string          `json:"Names"`
+	ID     string            `json:"Id"`
+	Labels map[string]string `json:"Labels"`
 }
 
-func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
-	io.WriteString(w, "Hello, HTTP!\n")
+func getImages() []Node {
+	ns := make([]Node, 0)
+
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
+	images, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, image := range images {
+		if err != nil {
+			panic(err)
+		}
+
+		n := Node{
+			Image:  image.Image,
+			Names:  image.Names,
+			ID:     image.ID,
+			Labels: image.Labels,
+		}
+
+		ns = append(ns, n)
+	}
+
+	return ns
+}
+
+func imageInfo(c echo.Context) error {
+	return c.JSONPretty(http.StatusOK, getImages(), "  ")
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", getRoot)
-	mux.HandleFunc("/hello", getHello)
 
-	err := http.ListenAndServe(":3333", mux)
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
-	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
-	}
+	e := echo.New()
+	e.GET("/", imageInfo)
+	e.Logger.Fatal(e.Start(":3333"))
 }
